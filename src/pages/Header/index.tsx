e2bd83a868cc9139027b7fe5a4944d58aa13fd1b
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2023-12-15 00:19:04 -0400

    feat: add demo Header and Resquest API demo

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,14 +1,30 @@
-import { useQuery } from "react-query";
+import { useRef } from "react";
+import { UserProps } from "../../interfaces/userProps";
 import { service } from "../../services";
 
 export function HeaderPage() {
-  const { data, isLoading } = useQuery("user", () => {
-    return service.getUserGitHub("victorlucass");
-  });
-  
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleFetchData = async () => {
+    const { data } = await service.getUserGitHub(inputRef.current ? inputRef.current.value.split(' ').join('') : "octocat");
+    const { avatar_url, login, location, followers, following } = data;
+    const user: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    console.log(user);
+  };
+
   return (
     <>
       <h1>Header</h1>
+      <input type="text" ref={inputRef} onKeyDown={(e) => e.key === "Enter" && handleFetchData()} />
+      <button type="submit" onClick={handleFetchData}>
+        Fetch
+      </button>
     </>
   );
 }
