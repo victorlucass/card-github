2b6463de773000e2881f1a6de6caae6f3cf948ac
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2023-12-18 21:32:16 -0400

    style: change value boxShadow

--- src/styles/theme/light/index.ts
+++ src/styles/theme/light/index.ts
@@ -5,7 +5,7 @@ export const themeLight: Theme = {
   secondary: "#b07dfb",
   backgroundLinearGradient: "linear-gradient(135deg, #8844ee, #b07dfb)",
   text: "#ffffff",
-  boxShadow: "-1rem 0 3rem #000",
+  boxShadow: "-1rem 0 3rem #b07dfb",
   stroke: "#B07DFB",
   backgroundH2: "linear-gradient(135deg, #fafbfc, #b07dfb)",
 };
